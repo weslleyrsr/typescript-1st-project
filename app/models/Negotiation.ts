e04c2953705133818fec281d8cf2cc413acc6e46
Weslley Rodrigo <weslley.rodriigo@gmail.com>
2022-03-19 18:39:56 -0300

    Improve security

--- app/models/Negotiation.ts
+++ app/models/Negotiation.ts
@@ -4,18 +4,26 @@ export class Negotiation {
     /*
         As we declared our variables as private on the constructor parameters, we don't need to set each one of them inside the constructor
         Getters not needed because we declared our variables as readonly
+
+        constructor (
+            private readonly date: Date,
+            private readonly quantity: number,
+            private readonly value: number
+        ) {
+            this._id = new Date().getTime();
+        }
     */
 
     constructor (
-        private readonly _date: Date,
-        private readonly _quantity: number,
-        private readonly _value: number
+        private _date: Date,
+        private _quantity: number,
+        private _value: number
     ) {
         this._id = new Date().getTime();
     }
 
     get date(): Date {
-        return this._date;
+        return new Date(this._date.getTime());
     }
 
     get quantity(): number {
