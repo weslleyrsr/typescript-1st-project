cfba78aaada8c68215dc0032ff0642c491558c7b
Weslley Rodrigo <weslley.rodriigo@gmail.com>
2022-03-20 21:17:18 -0300

    Improve enum declaration

--- app/enums/week-days.ts
+++ app/enums/week-days.ts
@@ -1,9 +1,9 @@
 export enum WeekDays {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+    SUNDAY = 0,
+    MONDAY = 1,
+    TUESDAY = 2,
+    WEDNESDAY = 3,
+    THURSDAY = 4,
+    FRIDAY = 5,
+    SATURDAY = 6
 }
