09bbd406acd33cc6291538163f682413a2af45d2
Weslley Rodrigo <weslley.rodriigo@gmail.com>
2022-03-20 23:09:19 -0300

    Improve readability

--- app/models/Negotiation.ts
+++ app/models/Negotiation.ts
@@ -3,19 +3,6 @@ import { WeekDays } from "../enums/week-days.js";
 export class Negotiation {
     private _id: number;
 
-    /*
-        As we declared our variables as private on the constructor parameters, we don't need to set each one of them inside the constructor
-        Getters not needed because we declared our variables as readonly
-
-        constructor (
-            private readonly date: Date,
-            private readonly quantity: number,
-            private readonly value: number
-        ) {
-            this._id = new Date().getTime();
-        }
-    */
-
     constructor (
         private _date: Date,
         private _quantity: number,
